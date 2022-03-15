81b85124ab35bb1c9e7f31867ad9ff87d41fca32
RidwanNur <nurridwan498@gmail.com>
2022-03-15 12:01:23 +0700

    m

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,6 @@ use App\Http\Controllers\DataLokasiController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Satpam\SatpamController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 /* Route::get('/', function () {
     return view('welcome');
